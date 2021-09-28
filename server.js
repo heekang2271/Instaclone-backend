09abd9ca28b9e93522681d4ea22fc67f704979ed
heekang2271 <gmlrkd3922@gmail.com>
2021-09-28 22:11:30 +0900

    영화 예제 끝

--- server.js
+++ server.js
@@ -1,56 +1,12 @@
-import { PrismaClient } from "@prisma/client";
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const client = new PrismaClient();
+require("dotenv").config();
 
-const typeDefs = gql`
-    type Movie {
-        id: Int!
-        title: String!
-        year: Int!
-        genre: String
-        createdAt: String!
-        updatedAt: String!
-    }
-    type Query {
-        movies: [Movie]
-        movie(id: Int!): Movie
-    }
-    type Mutation {
-        createMovie(title: String!, year: Int!, genre: String): Movie
-        deleteMovie(id: Int!): Movie
-        updateMovie(id: Int!, title: String, year: Int, genre: String): Movie
-    }
-`;
-
-const resolvers = {
-    Query: {
-        movies: () => client.movie.findMany(),
-        movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-    },
-    Mutation: {
-        createMovie: (_, { title, year, genre }) =>
-            client.movie.create({
-                data: {
-                    title,
-                    year,
-                    genre,
-                },
-            }),
-        deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
-        updateMovie: (_, { id, title, year, genre }) =>
-            client.movie.update({
-                where: { id },
-                data: { title, year, genre },
-            }),
-    },
-};
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+const server = new ApolloServer({ schema });
 
 server
     .listen()
-    .then(() => console.log("Server is running on http://localhost:4000"));
+    .then(() =>
+        console.log(`Server is running on localhost:${process.env.PORT}/`)
+    );
